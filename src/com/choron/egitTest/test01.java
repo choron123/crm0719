33f71ab5d3f52a0d611e0813602139366d4404f9
choron <choron@123.com>
2020-05-26 17:12:41 +0800

    update gitTest 02

--- src/com/choron/egitTest/test01.java
+++ src/com/choron/egitTest/test01.java
@@ -3,6 +3,7 @@ package com.choron.egitTest;
 public class test01 {
 	public static void main(String[]args) {
 		System.out.println("ÄãºÃÔ¶³Ì²Ö¿âgithub");
+		System.out.println("ÄãºÃÔ¶³Ì²Ö¿âgithub02");
 	}
 
 }
