f1a54a51a4e86fbc414bf18ba9ac26f6083e16e4
choron123 <65941297+choron123@users.noreply.github.com>
2020-05-26 17:30:46 +0800

    update by github
    update by github

---
+++ SRC/COM/合唱/平等测试/test01.java
@@ -0,0 +1,10 @@
+package com.choron.egitTest;
+
+public class test01 {
+	public static void main(String[]args) {
+		System.out.println("ÄãºÃÔ¶³Ì²Ö¿âgithub");
+		System.out.println("ÄãºÃÔ¶³Ì²Ö¿âgithub02");
+		System.out.println("github=3");
+	}
+
+}
